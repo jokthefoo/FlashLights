3fb156339ab0831e0a4ddcb05e1b8eb0d8f6ab84
Alex Kofford <alexkofford@gmail.com>
2018-10-17 10:51:54 -0600

    Initial upload

---
+++ FlashLightTesting/Assets/Controls.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public Light yellowLight;
+    public Light blueLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            blueLight.transform.position = new Vector3(blueLight.transform.position.x, blueLight.transform.position.y +.1f, blueLight.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            blueLight.transform.position = new Vector3(blueLight.transform.position.x - .1f, blueLight.transform.position.y, blueLight.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            blueLight.transform.position = new Vector3(blueLight.transform.position.x, blueLight.transform.position.y - .1f, blueLight.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            blueLight.transform.position = new Vector3(blueLight.transform.position.x  + .1f, blueLight.transform.position.y, blueLight.transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            yellowLight.transform.position = new Vector3(yellowLight.transform.position.x, yellowLight.transform.position.y + .1f, yellowLight.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            yellowLight.transform.position = new Vector3(yellowLight.transform.position.x - .1f, yellowLight.transform.position.y, yellowLight.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            yellowLight.transform.position = new Vector3(yellowLight.transform.position.x, yellowLight.transform.position.y - .1f, yellowLight.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            yellowLight.transform.position = new Vector3(yellowLight.transform.position.x + .1f, yellowLight.transform.position.y, yellowLight.transform.position.z);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if(blueLight.intensity == 0)
+            {
+                blueLight.intensity = 1;
+            }
+            else
+            {
+                blueLight.intensity = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (yellowLight.intensity == 0)
+            {
+                yellowLight.intensity = 1;
+            }
+            else
+            {
+                yellowLight.intensity = 0;
+            }
+        }
+    }
+}
---
+++ FlashLightTesting/Assets/LightSource.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class LightSource : MonoBehaviour {
+
+    public Object[] baseMats;
+    public bool blue;
+    public Object[] greenMats;
+    public Light light1;
+    public Light light2;
+
+	// Use this for initialization
+	void Start () {
+        if(blue)
+        {
+            baseMats = Resources.LoadAll("Mats/BlueMats", typeof(Material));
+        }
+        else
+        {
+            baseMats = Resources.LoadAll("Mats/YellowMats", typeof(Material));
+        }
+        greenMats = Resources.LoadAll("Mats/GreenMats", typeof(Material));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        foreach(Object reveal in baseMats)
+        {
+            ((Material)reveal).SetVector("_LightPosition", light1.transform.position);
+            ((Material)reveal).SetVector("_LightDirection", -light1.transform.forward);
+            ((Material)reveal).SetFloat("_LightAngle", light1.spotAngle);
+            ((Material)reveal).SetFloat("_LightIntensity", light1.intensity);
+        }
+
+        foreach (Object revealBoth in greenMats)
+        {
+            ((Material)revealBoth).SetVector("_LightPosition", light1.transform.position);
+            ((Material)revealBoth).SetVector("_LightDirection", -light1.transform.forward);
+            ((Material)revealBoth).SetFloat("_LightAngle", light1.spotAngle);
+            ((Material)revealBoth).SetFloat("_LightIntensity", light1.intensity);
+            ((Material)revealBoth).SetVector("_LightPosition2", light2.transform.position);
+            ((Material)revealBoth).SetVector("_LightDirection2", -light2.transform.forward);
+            ((Material)revealBoth).SetFloat("_LightAngle2", light2.spotAngle);
+            ((Material)revealBoth).SetFloat("_LightIntensity2", light2.intensity);
+        }
+    }
+}
