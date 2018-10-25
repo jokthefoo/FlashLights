08026a05e53bc1793909a881704f55f9df1589e5
Alex Kofford <alexkofford@gmail.com>
2018-10-25 10:44:48 -0600

    Got putting objects in circle working

---
+++ FlashLightTesting/Assets/Scripts/ObjectInside.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInside : MonoBehaviour {
+
+    private bool activated = false;
+    public string targetName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == targetName && !activated)
+        {
+            Debug.Log("INSIDE CIRCLE!!!");
+            activated = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == targetName && activated)
+        {
+            Debug.Log("LEAVING CIRCLE!!!");
+            activated = false;
+        }
+    }
+}
