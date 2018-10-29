15934cd3ecc72c1458bafe594b419cd5864678cc
Srija <u1209446@utah.edu>
2018-10-29 15:45:54 -0600

    Added in scripts

---
+++ FlashLightTesting/Assets/Scripts/Follow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : Invokable {
+
+    public GameObject FollowTarget;
+    public float FollowSpeed;
+
+    public override void Invoke()
+    {
+        // follows a straight-line path for now. Need to add pathfinding with obstacles and stuff?
+        transform.position = Vector3.MoveTowards(transform.position, FollowTarget.transform.position, FollowSpeed);
+    }
+}
---
+++ FlashLightTesting/Assets/Scripts/IfNotSeen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfNotSeen : MonoBehaviour {
+
+    Invokable invokable;
+    public Camera cameraToCheck;
+
+	// Use this for initialization
+	void Start () {
+        invokable = gameObject.GetComponent<Invokable>();
+        cameraToCheck = FindObjectOfType<Camera>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 screenPoint = cameraToCheck.WorldToViewportPoint(transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        if (!onScreen)
+            invokable.Invoke();
+    }
+}
