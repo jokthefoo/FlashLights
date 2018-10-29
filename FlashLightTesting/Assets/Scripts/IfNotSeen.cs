c4c248d21712cf2d072a05c292049b51d35a359b
Srija <u1209446@utah.edu>
2018-10-29 15:50:19 -0600

    Mannequin following test

--- FlashLightTesting/Assets/Scripts/IfNotSeen.cs
+++ FlashLightTesting/Assets/Scripts/IfNotSeen.cs
@@ -6,16 +6,18 @@ public class IfNotSeen : MonoBehaviour {
 
     Invokable invokable;
     public Camera cameraToCheck;
+    public bool enable = false;
 
 	// Use this for initialization
 	void Start () {
         invokable = gameObject.GetComponent<Invokable>();
         cameraToCheck = FindObjectOfType<Camera>();
-
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (!enable)
+            return;
         Vector3 screenPoint = cameraToCheck.WorldToViewportPoint(transform.position);
         bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
         if (!onScreen)
