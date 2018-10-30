9dc78148e2ccd38f4abb573e6e68c32b0c4ea909
Srija <u1209446@utah.edu>
2018-10-30 14:58:08 -0600

    No more bouncing mannequins hopefully

--- FlashLightTesting/Assets/Scripts/Follow.cs
+++ FlashLightTesting/Assets/Scripts/Follow.cs
@@ -8,21 +8,39 @@ public class Follow : Invokable {
     public float FollowSpeed;
     AudioClip scraping;
     AudioSource mannequinSounds;
+    GameObject[] objectsToStayAwayFrom;
+    public float distance;
 
     private void Start()
     {
         mannequinSounds = GetComponent<AudioSource>();
         scraping = Resources.Load<AudioClip>("Sounds/Mannequin Slow");
+        objectsToStayAwayFrom = GameObject.FindGameObjectsWithTag("Mannequin");
     }
 
     public override void Invoke()
     {
         // follows a straight-line path for now. Need to add pathfinding with obstacles and stuff?
         float dist = Vector3.Distance(transform.position, FollowTarget.transform.position);
+        Vector3 pushback = new Vector3();
         if(dist > 1)
         {
-            transform.position = Vector3.MoveTowards(transform.position, FollowTarget.transform.position, FollowSpeed);
+            foreach (GameObject obj in objectsToStayAwayFrom)
+            {
+                if (obj != gameObject)
+                {
+                    dist = Vector3.Distance(transform.position, obj.transform.position);
+                    if (dist < distance)
+                    {
+                        pushback += (transform.position - obj.transform.position).normalized;
+                    }
+
+                }
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, FollowTarget.transform.position, FollowSpeed) + new Vector3(pushback.x,0,0);
             transform.LookAt(FollowTarget.transform);
+            
             if (!mannequinSounds.isPlaying)
                 mannequinSounds.PlayOneShot(scraping);
         }
