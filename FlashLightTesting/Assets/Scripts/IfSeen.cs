77bb75f265c27019d988a93e3ffe3f92df8c7cd2
Srija <u1209446@utah.edu>
2018-10-29 21:13:56 -0600

    working on ifseen invokation

---
+++ FlashLightTesting/Assets/Scripts/IfSeen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfSeen : MonoBehaviour {
+
+    public Invokable[] invokables;
+    public Camera cameraToCheck;
+    public bool enable = false;
+
+	// Use this for initialization
+	void Start () {
+        if(invokables == null)
+            invokables = gameObject.GetComponents<Invokable>();
+        cameraToCheck = FindObjectOfType<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!enable)
+            return;
+        Vector3 screenPoint = cameraToCheck.WorldToViewportPoint(transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        if (!onScreen)
+            foreach(Invokable i : invokables)
+    }
+}
