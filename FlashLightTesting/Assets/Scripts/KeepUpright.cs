8a398263cbfe3ca246fb84b953310cc141bed91b
Srija <u1209446@utah.edu>
2018-10-31 20:01:35 -0600

    Upright-snapping and Follow on entering room

---
+++ FlashLightTesting/Assets/Scripts/KeepUpright.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepUpright : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void keepUpright()
+    {
+        transform.rotation = Quaternion.Euler(0,transform.rotation.y,0);
+    }
+}
---
+++ FlashLightTesting/Assets/Scripts/OnEnterTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnterTrigger : MonoBehaviour {
+
+    public GameObject[] mannies;
+    bool triggered = false;
+
+    // Use this for initialization
+    void Start () {
+        mannies = GameObject.FindGameObjectsWithTag("Mannequin");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindGameObjectWithTag("Player").transform.position.z > 22.864)
+            trigger();
+
+	}
+
+    private void trigger()
+    {
+        if (triggered)
+            return;
+        for (int i = 0; i < mannies.Length; i++)
+        {
+            mannies[i].GetComponent<EnableTrigger>().Invoke();
+        }
+        triggered = true;
+    }
+}
